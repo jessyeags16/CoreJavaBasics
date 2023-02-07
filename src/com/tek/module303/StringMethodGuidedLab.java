6fbacb30b806469fca4b629722d9ac59ef8122dd
LAPTOP-2V5E6HUA\Jessica Yeager <jessicayeager>
2023-02-07 13:01:02 -0500

    String Guided Lab

---
+++ src/com/tek/module303/StringMethodGuidedLab.java
@@ -0,0 +1,48 @@
+package com.tek.module303;
+
+public class StringMethodGuidedLab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * LAB OBJECTIVE: 
+		 * In this lab, you will understand and demonstrate the inbuilt methods in the String class.
+		 *  These methods help you to manipulate Strings type data. 
+		 */
+		
+		// 1. The length() method
+		
+		String s1 = "Java";
+		String s2 = "";
+		
+		System.out.println(s1.length());
+		System.out.println(s2.length());
+		System.out.println("Java".length());
+		System.out.println("Java\n".length()); //new line gets counted as part of length
+		System.out.println("Learn Java".length());
+		
+		// 2. IsEmpty() method
+		/*
+		 * This method checks whether the String contains anything or not.
+		 * If the Java String is empty, it returns true or false. For example:
+		 */
+		
+		String str1 = "";
+		String str2 = "Hello";
+		System.out.println(str1.isEmpty());
+		System.out.println(str2.isEmpty()); 
+		
+		// 3. Trim() method
+		/*
+		 * The Java string trim() method removes the leading and trailing spaces. 
+		 * It checks the Unicode value of the space character (‘\u0020’) before and after the string. 
+		 * If it exists, then remove the spaces and return the omitted string. For example:
+		 */
+		
+		String s3 = "   Hello   ";
+		System.out.println(s3 + "How are you?");
+		System.out.println(s3.trim() + "How are you?");
+	}
+
+}
